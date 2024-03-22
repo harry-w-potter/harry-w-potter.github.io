e91e76ea5e67fb01dbb2b5b04ed660db5ffc3c4c
Harry-W-Potter <136218302+Harry-W-Potter@users.noreply.github.com>
2024-03-22 18:27:16 +0800

    add

--- chat.js
+++ chat.js
@@ -1,18 +1,3 @@
-// Fetch the JSON file containing the messages
-fetch('messages.json')
-  .then(response => response.json())
-  .then(data => {
-    // Store the messages in a variable
-    const messages = data.messages;
-
-    // Display the messages in the chat window
-    messages.forEach(message => {
-      const li = document.createElement('li');
-      li.textContent = message;
-      document.getElementById('chat-messages').appendChild(li);
-    });
-  });
-
 // Add an event listener to the chat form
 document.getElementById('chat-form').addEventListener('submit', e => {
   e.preventDefault();
@@ -27,4 +12,20 @@ document.getElementById('chat-form').addEventListener('submit', e => {
 
   // Clear the input field
   document.getElementById('chat-input').value = '';
+
+  // Add the message to the JSON file
+  fetch('messages.json')
+    .then(response => response.json())
+    .then(data => {
+      data.messages.push(message);
+
+      // Update the JSON file with the new message
+      fetch('messages.json', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    });
 });
